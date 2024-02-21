bf7551eab9ffb0efa9f834532b5e8d450ca3e282
Shanidhchillar <shanidh.ahammed@chillarcards.com>
2024-02-21 10:07:29 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+
+import { AppComponent } from './app.component';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    NavbarModule,
+    FooterModule,
+    SidebarModule,
+    AppRoutingModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
